9730f523f69ac91228a537fa3225831db03da3ad
Ricardo <ricardo.profe.informatica@gmail.com>
2022-11-25 17:33:49 +0100

    first commit

---
+++ company_form.php
@@ -0,0 +1,33 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Company </title>
+    <meta charset="UTF-8">
+    <meta name="author" content="">
+    <link type="text/css" rel="stylesheet" href="main.css">
+</head>
+
+<body>
+<h1> Company </h1>
+<form> <!-- Complete the post and method attributes -->
+    <label>ID</label>
+    <input type="text" name="id" readonly value=""> <br>
+
+    <label>Company name</label>
+    <input type="text" name="name" value=""> <br>
+
+    <label>Company phone</label>
+    <input type="text" name="phone" value=""> <br>
+
+    <label>Company e-mail</label>
+    <input type="text" name="email" value=""> <br>
+
+    <input type="submit" name="submit" value="Save">
+    <input type="submit" name="export" value="Export as CSV">
+
+</form>
+
+</body>
+</html>
